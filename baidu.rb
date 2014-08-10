ea35ba4750d94aa5c5c83857504784568eae0b1e
Damon Da Tong <tongda@outlook.com>
2014-08-11 01:37:43 +0800

    add wandoujia

--- baidu.rb
+++ baidu.rb
@@ -9,6 +9,7 @@ File::open('ranks.txt', 'r:utf-8') do |file|
     puts line
     items = line.split '$$'
     app = {:rank => items[0].strip, :name => items[1].strip, :corp => items[2].strip}
+    app[:downloads] = {}
     apps.push app
   end
 end
@@ -17,14 +18,35 @@ browser = Watir::Browser.new :chrome
 browser.goto 'http://as.baidu.com'
 
 # search
-apps.map! do |app|
+apps.each do |app|
   puts "trying to process #{app[:name]}..."
 
   browser.text_field(:id => 'word').when_present.set app[:name]
   browser.button(:id => 'rd-search_submit').click
 
   if browser.ul(:class => 'dataList cls').present? then
-    app[:downloads] = browser.ul(:class => 'dataList cls').li.div(:class => 'right').span(:class => 'date').text
-    puts "#{app[:name]} #{app[:downloads]}"
+    app[:downloads][:baidu] = browser.ul(:class => 'dataList cls').li.div(:class => 'right').span(:class => 'date').text
+  end
+end
+
+puts apps
+
+# temp put 360 here
+browser.goto 'http://www.wandoujia.com/apps'
+
+apps.each do |app|
+  browser.text_field(:id => 'j-search-input').when_present.set app[:name]
+  browser.button(:value => '搜索').click
+
+  if browser.span(:class => 'install-count').present? then
+    app[:downloads][:wandoujia] = browser.span(:class => 'install-count').text
+  end
+end
+
+browser.close
+
+File.open('stats.txt', 'w') do |file|
+  apps.each do |app|
+    file.puts "#{app[:rank]}$$#{app[:name]}$$#{app[:corp]}$$#{app[:downloads][:baidu]}$$#{app[:downloads][:wandoujia]}"
   end
 end
