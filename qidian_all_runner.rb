16c2c4532f6e19e4a4ffae6ac73e7aad212f4674
Tong Damon Da <tongda@outlook.com>
2014-08-25 22:58:27 +0800

    add 打赏

--- qidian_all_runner.rb
+++ qidian_all_runner.rb
@@ -29,23 +29,4 @@ end
 
 mapper.map_all
 
-# File.open("qidian.all.txt", "w") do |file|
-#   mapper.books.each do |book|
-#     file.puts "#{book[:rank]} $$ #{book[:category]} $$ #{book[:name]} $$ #{book[:month_votes]} $$ #{book[:author]} $$ #{book[:update_time]} $$ #{book[:url]}"
-#   end
-# end
-
-# mapper.gen_details
-
-# File.open("qidian.all.all.txt", "w") do |file|
-#   mapper.books.each do |book|
-#     file.puts "#{book[:rank]} $$ #{book[:category]} $$ \
-#     #{book[:name]} $$ #{book[:month_votes]} $$ #{book[:author]} $$ \
-#     #{book[:update_time]} $$ #{book[:lv]} $$ #{book[:url]} $$ \
-#     #{book[:detail][:quality]} $$ #{book[:detail][:total_hit]} $$ #{book[:detail][:month_hit]} $$ \
-#     #{book[:detail][:week_hit]} $$ #{book[:detail][:total_recommand]} $$ #{book[:detail][:month_recommand]} $$ \
-#     #{book[:detail][:week_recommand]} $$ #{book[:detail][:word_completed]}"
-#   end
-# end
-
 mapper.browser.close
---
+++ qidian_dashang_top.rb
@@ -0,0 +1,74 @@
+require 'watir-webdriver'
+require 'date'
+
+filename = "qidian.dashang.#{DateTime.now.strftime('%s')}.txt"
+
+def from_line(line)
+  parts = line.split ","
+  return {
+    rank: parts[0].strip,
+    name: parts[2].strip,
+    url: parts[7].strip,
+  }
+end
+
+def open_browser(times = 0, &p)
+  browser = Watir::Browser.new :phantomjs
+  begin
+    p.call browser
+  rescue Exception => e
+    puts e.message
+
+    times += 1
+    puts "retry for #{times} times"
+
+    if times < 2
+      open_browser times do
+        p.call browser
+      end
+    else
+      puts "give up"
+    end
+  else
+    browser.close
+  end
+end
+
+def map_page(book)
+  open_browser do |br|
+    br.goto book[:url]
+    br.link(:id => 'hdtabs02').hover
+    br.div(:id => 'award').when_present do
+      t = br.div(:id => 'award').div(:class => "ballot_data").text
+      puts t
+      parts = t.split '  '
+
+      book[:ds_time] = DateTime.now
+      book[:ds_week] = parts[0]
+      book[:ds_day] = parts[1]
+    end
+  end
+  yield book
+end
+
+mod = ARGV[0].to_i
+rem = ARGV[1].to_i
+
+puts "mod: #{mod} rem: #{rem}"
+
+File.open "qidian_top5000.csv", 'r:utf-8' do |in_file|
+  puts "qidian_top5000.csv opened"
+  File.open filename, 'w' do |out_file|
+    in_file.each_line do |line|
+      book = from_line line
+      if book[:rank].to_i % mod == rem
+        puts "processing line: #{line}"
+        map_page book do |b|
+          result = "#{b[:rank]},#{b[:name]},#{b[:url]},#{b[:ds_time].to_s},#{b[:ds_week]},#{b[:ds_day]}"
+          out_file.puts line
+          puts result
+        end
+      end
+    end
+  end
+end
